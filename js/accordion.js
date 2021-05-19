f4611950778396d66eda05f4fe43769c95c28048
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-05-19 14:31:35 +0300

    Добавляет плавный скролл и табы

--- js/accordion.js
+++ js/accordion.js
@@ -15,14 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   featureLinkElems.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-      featureSubElems.forEach((featureSubElem) => {
-        featureSubElem.classList.add("hidden");
-      });
-      featureLinkElems.forEach((featureLinkElem) => {
-        featureLinkElem.classList.remove("feature__link_active");
-      });
-      featureSubElems[index].classList.remove("hidden");
-      btn.classList.add("feature__link_active");
+      if (btn.classList.contains("feature__link_active")) {
+        btn.classList.remove("feature__link_active");
+        featureSubElems[index].classList.add("hidden");
+      } else {
+        featureSubElems.forEach((featureSubElem) => {
+          featureSubElem.classList.add("hidden");
+        });
+        featureLinkElems.forEach((featureLinkElem) => {
+          featureLinkElem.classList.remove("feature__link_active");
+        });
+        featureSubElems[index].classList.remove("hidden");
+        btn.classList.add("feature__link_active");
+      }
     });
   });
 });
